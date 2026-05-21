49e84bdb71bd453d25b8ab9a51790d5ec49a8329
Anirudra Shrestha <anirudrashrestha123@gmail.com>
2026-05-21 13:52:48 +0545

    Comments on the codes

--- tests/DatePicker.spec.js
+++ tests/DatePicker.spec.js
@@ -4,13 +4,16 @@ test('Date Picker', async({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/2018/09/automation-form.html')
 
+    //Hardcoded date in input
     //await page.fill('#datepicker', '05/20/2026')
+
+    //Date selected from Date picker calender using loops
     const year="2026";
     const month="June";
     const day="15";
 
     await page.click('#datepicker');
-
+    //Selecting year and month
     while(true){
         const currentMonth=await page.locator('.ui-datepicker-month').textContent();
         const currentYear=await page.locator('.ui-datepicker-year').textContent();
@@ -21,7 +24,7 @@ test('Date Picker', async({page})=>{
 
         await page.click('.ui-icon.ui-icon-circle-triangle-e');
     }
-
+    //Selecting date
     const dates=await page.$$('.ui-state-default');
     for(const dt of dates){
         const number=await dt.textContent();
